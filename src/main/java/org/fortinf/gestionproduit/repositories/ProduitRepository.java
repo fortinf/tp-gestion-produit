af58a910c2e33fa85b28d1f5868913a7f9c6af9f
François Fortin <fortinf@gmail.com>
2017-10-14 14:14:42 +0200

    Ajout de l'annotation spring

--- src/main/java/org/fortinf/gestionproduit/repositories/ProduitRepository.java
+++ src/main/java/org/fortinf/gestionproduit/repositories/ProduitRepository.java
@@ -2,6 +2,8 @@ package org.fortinf.gestionproduit.repositories;
 
 import org.fortinf.gestionproduit.domain.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 }
