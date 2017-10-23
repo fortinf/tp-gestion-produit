5234ec33072fa7844fbfb8ee19fbbab90eb7cd58
François Fortin <fortinf@gmail.com>
2017-10-23 18:10:43 +0200

    Pagination du tableau de recherche

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,7 +3,7 @@ var module = angular.module('ProduitApp', ['ngResource']);
 module.factory('Produit', ['$resource', function($resource){
     return $resource('/produits/:produitId', {produitId: '@id'},
         {
-            'search' : {url: '/produits/search', method: 'GET', params: {'exp':'', page:0, size:5,isArray: false}}
+            'search' : {url: '/produits/search', method: 'GET', params: {'exp':'@exp', page: '@page', size: '@size'}, isArray: false}
         }
     );
 }]);
@@ -11,8 +11,27 @@ module.factory('Produit', ['$resource', function($resource){
 
 module.controller('ProduitCtrl', function($scope, Produit) {
 
-    $scope.produits = Produit.search();
+    $scope.exp = '';
+    $scope.currentPage = 0;
+    $scope.tableSize = 5;
+    $scope.tabPagination = [];
+    $scope.nouvelleRecherche = function() {
+        $scope.currentPage = 0;
+        rechercherProduit();
+    }
+    $scope.goToPage = function(page) {
+        $scope.currentPage = page;
+        rechercherProduit();
+    }
+
+    function rechercherProduit(){
+        Produit.search({exp: $scope.exp, page: $scope.currentPage, size: $scope.tableSize}, function(produits) {
+            $scope.produits = produits;
+            $scope.tabPagination = new Array(produits.totalPages);
+        });
+    }
 
 });
+
 
 
