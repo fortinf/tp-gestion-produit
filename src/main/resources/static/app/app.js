902a7e0f1612084491716c7e1994e6f11642ed96
François Fortin <fortinf@gmail.com>
2017-10-25 12:31:34 +0200

    AngularJs : configuration des routes

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,6 +1,44 @@
-var module = angular.module('ProduitApp', ['ngResource']);
+/**
+ * Module "ProduitApp" de l'application AngularJs
+ * @type {angular.Module}
+ */
+var module = angular.module('ProduitApp', ['ngResource', 'ui.router']);
 
+module.config(['$stateProvider', router]);
 
+/**
+ * Configuration des routes de l'application "ProduitApp"
+ * @param $stateProvider
+ */
+function router($stateProvider) {
+    var homeProduitState = {
+        name : 'homeProduitState',
+        url : '/produit/home',
+        template :'<h1>{{ Home.message }}</h1>',
+        controller : function () {
+            this.message = 'Bienvenue';
+        },
+        controllerAs : 'Home'
+    };
+
+    var searchProduitState = {
+        name : 'searchProduitState',
+        url : '/produit/search',
+        templateUrl : 'app/views/produit/search.html',
+        controller : 'ProduitController'
+    };
+
+    var newProduitState = {
+        name : 'newProduitState',
+        url : '/produit/new',
+        templateUrl : 'app/views/produit/new.html',
+        controller : 'ProduitController'
+    };
+
+    $stateProvider.state(homeProduitState);
+    $stateProvider.state(searchProduitState);
+    $stateProvider.state(newProduitState);
+}
 
 
 
