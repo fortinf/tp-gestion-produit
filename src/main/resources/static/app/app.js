81c1867cf2bbe5e9a8facd301cbda83875b13e94
François Fortin <fortinf@gmail.com>
2017-10-27 15:27:45 +0200

    AngularJs : nouveau produit
    gestion des msg d'erreurs
    ajout d'une alerte de confirmation d'ajout

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,49 +1,52 @@
-/**
- * Module "ProduitApp" de l'application AngularJs
- * @type {angular.Module}
- */
-var module = angular.module('ProduitApp', ['ngResource', 'ui.router', 'ngMessages']);
+(function () {
+    'use strict';
 
-module.config(['$stateProvider', '$locationProvider', router]);
+    /**
+     * Module "ProduitApp" de l'application AngularJs
+     * @type {angular.Module}
+     */
+    var module = angular.module('ProduitApp', ['ngResource', 'ui.router', 'ngMessages', 'ui.bootstrap']);
 
-module.controller('MainController', ['$rootScope', '$location', function($rootScope, $location) {
-    $rootScope.path = $location.path();
-}]);
+    module.config(['$stateProvider', '$locationProvider', router]);
 
-/**
- * Configuration des routes de l'application "ProduitApp"
- * @param $stateProvider
- */
-function router($stateProvider, $locationProvider) {
+    module.controller('MainController', ['$rootScope', '$location', function ($rootScope, $location) {
+        $rootScope.path = $location.path();
+    }]);
 
-    var homeProduitState = {
-        name : 'homeProduitState',
-        url : '/produit/home',
-        templateUrl :'app/views/produit/home.html',
-        controller : 'ProduitController'
-    };
+    /**
+     * Configuration des routes de l'application "ProduitApp"
+     * @param $stateProvider
+     */
+    function router($stateProvider, $locationProvider) {
 
-    var searchProduitState = {
-        name : 'searchProduitState',
-        url : '/produit/search',
-        templateUrl : 'app/views/produit/search.html',
-        controller : 'ProduitController'
-    };
+        var homeProduitState = {
+            name: 'homeProduitState',
+            url: '/produit/home',
+            templateUrl: 'app/views/produit/home.html',
+            controller: 'ProduitController'
+        };
 
-    var newProduitState = {
-        name : 'newProduitState',
-        url : '/produit/new',
-        templateUrl : 'app/views/produit/new.html',
-        controller : 'ProduitController'
-    };
+        var searchProduitState = {
+            name: 'searchProduitState',
+            url: '/produit/search',
+            templateUrl: 'app/views/produit/search.html',
+            controller: 'ProduitController'
+        };
 
-    $stateProvider.state(homeProduitState);
-    $stateProvider.state(searchProduitState);
-    $stateProvider.state(newProduitState);
+        var newProduitState = {
+            name: 'newProduitState',
+            url: '/produit/new',
+            templateUrl: 'app/views/produit/new.html',
+            controller: 'ProduitController'
+        };
 
-    $locationProvider.html5Mode(true);
-}
+        $stateProvider.state(homeProduitState);
+        $stateProvider.state(searchProduitState);
+        $stateProvider.state(newProduitState);
 
+        $locationProvider.html5Mode(true);
+    }
+})();
 
 
 
--- src/main/resources/static/app/controllers/produitController.js
+++ src/main/resources/static/app/controllers/produitController.js
@@ -1,53 +1,79 @@
-/**
- * Controller "ProduitController"
- */
-module.controller('ProduitController', ['$rootScope', '$scope', '$location', 'produitService', function($rootScope, $scope, $location, produitService) {
-
-    var ProduitResource = produitService.getResource();
-
-    $scope.exp = '';
-    $scope.currentPage = 0;
-    $scope.tableSize = 5;
-    $scope.tabPagination = [];
-
-    $rootScope.path = $location.path();
+(function () {
+    'use strict';
 
 
     /**
-     * Lance une nouvelle recherche (avec mot clé ou pas)
+     * Controller "ProduitController"
      */
-    $scope.nouvelleRecherche = function() {
+    angular.module('ProduitApp').controller('ProduitController',
+        ['$rootScope', '$scope', '$location', '$state', 'produitService',
+            function ($rootScope, $scope, $location, $state, produitService) {
+
+        var ProduitResource = produitService.getResource();
+
+        $scope.exp = '';
         $scope.currentPage = 0;
-        rechercherProduit();
-    }
+        $scope.tableSize = 5;
+        $scope.tabPagination = [];
+        $scope.produitToCreate = new ProduitResource();
+        $scope.alerts = [];
 
-    /**
-     * Lance une nouvelle recherche (avec un mot clé ou pas + le n° d'une page)
-     * @param page
-     */
-    $scope.goToPage = function(page) {
-        $scope.currentPage = page;
-        rechercherProduit();
-    }
+        $rootScope.path = $location.path();
 
-    /**
-     * Communique avec le server pour créer un nouveau produit
-     */
-    $scope.newProduit = function() {
-        if ($scope.newProduitForm.$valid) {
-            var produit = new ProduitResource();
-            produit.libelle = $scope.produit.libelle;
-            produit.description = $scope.produit.description;
-            produit.prix = $scope.produit.prix;
-            produit.$save();
+
+        /**
+         * Lance une nouvelle recherche (avec mot clé ou pas)
+         */
+        $scope.nouvelleRecherche = function () {
+            $scope.currentPage = 0;
+            rechercherProduit();
         }
-    }
 
-}]);
+        /**
+         * Lance une nouvelle recherche (avec un mot clé ou pas + le n° d'une page)
+         * @param page
+         */
+        $scope.goToPage = function (page) {
+            $scope.currentPage = page;
+            rechercherProduit();
+        }
 
-function rechercherProduit(){
-    ProduitResource.search({exp: $scope.exp, page: $scope.currentPage, size: $scope.tableSize}, function(produits) {
-        $scope.produits = produits;
-        $scope.tabPagination = new Array(produits.totalPages);
-    });
-}
+        /**
+         * Si le formulaire est valide, communique avec le server pour créer un nouveau produit
+         */
+        $scope.newProduit = function () {
+            if ($scope.newProduitForm.$valid) {
+                var promise = $scope.produitToCreate.$save();
+                promise.then(function () {
+                    $scope.alerts = [
+                        {type: 'success', msg: 'Création du produit réalisée avec succès!'}
+                    ];
+                    $scope.produitToCreate = new ProduitResource();
+                    //$state.reload();
+                });
+            }
+        }
+
+
+        /*
+            $scope.users = User.query(paramètres);
+            $scope.users.$promise.then(callback en cas de succès).catch(callback en cas d'échec);
+         */
+
+        $scope.closeAlert = function (index) {
+            $scope.alerts.splice(index, 1);
+        };
+
+        function rechercherProduit() {
+            ProduitResource.search({
+                exp: $scope.exp,
+                page: $scope.currentPage,
+                size: $scope.tableSize
+            }, function (produits) {
+                $scope.produits = produits;
+                $scope.tabPagination = new Array(produits.totalPages);
+            });
+        }
+    }]);
+
+})();
